0e0be8b858d187ab1551663c14127b0da908eb4f
Oscuriosiy <tony.oscuriosity@gmail.om>
2021-02-11 18:38:51 +0000

    motor control rewrite.

--- src/scripts/motorControl_node.py
+++ src/scripts/motorControl_node.py
@@ -5,28 +5,28 @@
 
 import rospy
 from DCMotor import DCMotor as DCM
-from std_msgs.msg import UInt64
+from std_msgs.msg import UInt64, Float64
 
+rospy.init_node('motorControl')
+prevStatedata = 0
 leftM = DCM(22, 23)
 
 
-def encCallback(enclf):
-    stateData = enclf
-
-def controlMotor(args):
-    rospy.init_node('motorControl')
-    rate = rospy.Rate(10)  # 10hz
-    rospy.Subscriber('/enc_lf', UInt64, encCallback)
-    pub = rospy.Publisher('state', UInt64, queue_size=10)
-    rospy.spin()
-    while not rospy.is_shutdown():
-        STATEdata = stateData
-        pub.publish(STATEdata)
-        rate.sleep()
+def encoderCB(enclf):
+    global prevStatedata
+    stateData = enclf.data - prevStatedata
+    prevStatedata = stateData
+    pub.publish(stateData)
 
 
-if __name__ == '__main__':
-    try:
-        controlMotor()
-    except rospy.ROSInterruptException:
-        pass
+def motorCB(pwm):
+    leftM.run(pwm)
+
+
+rate = rospy.Rate(10)  # 10hz
+encoderSub = rospy.Subscriber('enc_lf', UInt64, encoderCB)
+motorSub = rospy.Subscriber('control_effort', Float64, motorCB)
+pub = rospy.Publisher('state', UInt64, queue_size=10)
+
+rospy.spin()
+rate.sleep()
