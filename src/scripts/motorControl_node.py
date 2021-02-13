c2ee16cc1bf520428c6a2716672449cdc2e7c444
Oscuriosiy <tony.oscuriosity@gmail.om>
2021-02-13 16:41:54 +0000

    test

--- src/scripts/motorControl_node.py
+++ src/scripts/motorControl_node.py
@@ -5,18 +5,18 @@
 
 import rospy
 from DCMotor import DCMotor as DCM
-from std_msgs.msg import UInt64, Float64
+from std_msgs.msg import Float64
 
 rospy.init_node('motorControl')
-prevStatedata = 0
+prevStateData = 0.0
 leftM = DCM(22, 23)
 
 
 def encoderCB(enclf):
-    global prevStatedata
-    stateData = enclf.data - prevStatedata
-    prevStatedata = stateData
-    pub.publish(stateData)
+    global prevStateData
+    stateData = enclf.data - prevStateData
+    prevStateData = stateData
+    statePub.publish(stateData)
 
 
 def motorCB(pwm):
@@ -24,9 +24,9 @@ def motorCB(pwm):
 
 
 rate = rospy.Rate(10)  # 10hz
-encoderSub = rospy.Subscriber('enc_lf', UInt64, encoderCB)
+encoderSub = rospy.Subscriber('enc_lf', Float64, encoderCB)
 motorSub = rospy.Subscriber('control_effort', Float64, motorCB)
-pub = rospy.Publisher('state', UInt64, queue_size=10)
+statePub = rospy.Publisher('state', Float64, queue_size=10)
 
 rospy.spin()
 rate.sleep()
