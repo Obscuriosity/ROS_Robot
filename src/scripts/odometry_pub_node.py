2dd0d338e60b3d93f7dc2377524427d37dcd56cf
Obscuriosity <tony.oscuriosity@gmail.om>
2021-03-15 12:12:03 +0000

    A little rearrangement of odometry pub node.

--- src/scripts/odometry_pub_node.py
+++ src/scripts/odometry_pub_node.py
@@ -79,70 +79,73 @@ class odometry:
         self._rBacprevStateData = encrb.data
         # print("---------Right Ticks BAC", self._rBacstateData)
 
+    def move_robot(self):
+        x = 0.0
+        y = 0.0
+        th = 0.0
 
-def main(self):
+        vx = 0.0
+        vy = 0.0
+        vth = 0.0
+
+        current_time = rospy.Time.now()
+        last_time = rospy.Time.now()
+
+        odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
+        odom_broadcaster = tf.TransformBroadcaster()
+        rate = rospy.Rate(0.1)
+
+        while not rospy.is_shutdown():
+            current_time = rospy.Time.now()
+            lvx = (2 * pi/self._leftTPR) * self._lstate
+            rvx = (2 * pi/self._rightTPR) * self._rstate
+            vx = (lvx + rvx) / 2
+            # compute odometry in a typical way given the velocities of the robot
+            dt = (current_time - last_time).to_sec()
+            delta_x = (vx * cos(th)) * dt
+            delta_y = (vx * sin(th)) * dt
+            delta_th = vth * dt
+
+            x += delta_x
+            y += delta_y
+            th += delta_th
+
+            # since all odometry is 6DOF we'll need a quaternion created from yaw
+            odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
+
+            # first, we'll publish the transform over tf
+            odom_broadcaster.sendTransform(
+                (x, y, 0.),
+                odom_quat,
+                current_time,
+                "base_link",
+                "odom"
+            )
+
+            # next, we'll publish the odometry message over ROS
+            odom = Odometry()
+            odom.header.stamp = current_time
+            odom.header.frame_id = "odom"
+
+            # set the position
+            odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+
+            # set the velocity
+            odom.child_frame_id = "base_link"
+            odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
+
+            # publish the message
+            odom_pub.publish(odom)
+
+            last_time = current_time
+            rate.sleep()
+
+
+def main():
     rospy.init_node('Odometry_node')
     rospy.loginfo("Odometry Node Started")
-    odometry()
-    x = 0.0
-    y = 0.0
-    th = 0.0
-
-    vx = 0.0
-    vy = 0.0
-    vth = 0.0
-
-    current_time = rospy.Time.now()
-    last_time = rospy.Time.now()
-
-    odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
-    odom_broadcaster = tf.TransformBroadcaster()
-    rate = rospy.Rate(0.1)
-
-    while not rospy.is_shutdown():
-        current_time = rospy.Time.now()
-        lvx = (2 * pi/self._leftTPR) * self._lstate
-        rvx = (2 * pi/self._rightTPR) * self._rstate
-        vx = (lvx + rvx) / 2
-        # compute odometry in a typical way given the velocities of the robot
-        dt = (current_time - last_time).to_sec()
-        delta_x = (vx * cos(th)) * dt
-        delta_y = (vx * sin(th)) * dt
-        delta_th = vth * dt
-
-        x += delta_x
-        y += delta_y
-        th += delta_th
-
-        # since all odometry is 6DOF we'll need a quaternion created from yaw
-        odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
-
-        # first, we'll publish the transform over tf
-        odom_broadcaster.sendTransform(
-            (x, y, 0.),
-            odom_quat,
-            current_time,
-            "base_link",
-            "odom"
-        )
-
-        # next, we'll publish the odometry message over ROS
-        odom = Odometry()
-        odom.header.stamp = current_time
-        odom.header.frame_id = "odom"
-
-        # set the position
-        odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
-
-        # set the velocity
-        odom.child_frame_id = "base_link"
-        odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
-
-        # publish the message
-        odom_pub.publish(odom)
-
-        last_time = current_time
-        rate.sleep()
+    odom = odometry()
+    odom.move_robot()
 
 
 if __name__ == '__main__':
